7a4e91e435ee0b50280b2a93f50adc826cd39129
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2021-08-16 11:10:02 -0300

    arquitetura_base_do_app
    esqueleto do app

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(130),
+        child: Container(
+          height: 130,
+          color: Colors.black12,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: "Olá, ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.orange,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: "Seja bem vindo ao nosso App",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.orange,
+                        ),
+                      )
+                    ]),
+              ),
+              subtitle: Text(
+                "Matenha-se sempre motivado!",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.orange,
+                ),
+              ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
